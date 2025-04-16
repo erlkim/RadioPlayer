e41801340b6fd19b78a227529c47c34ce4cb27b1
Erlkim <99765959+erlkim@users.noreply.github.com>
2025-04-17 00:01:41 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ const RADIO_NAME = 'Game! Radio 1';
 var API_SERVICE = 'deezer';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR ETC.... DOES NOT SUPPORT HLS
-const URL_STREAMING = 'https://stream-51.zeno.fm/cfhkm5fs1uhvv?zs=HOu6hxV1SG-7iGi9WGVTqQ';
+const URL_STREAMING = 'https://stream.zeno.fm/n4gzbe9ufzzuv';
 
 //NOW PLAYING API.
-const API_URL = 'https://prod-api.radioapi.me/streamtitle/8ce17c30-2fc5-4f1d-a6d8-4ac3ca9b35b6';
+const API_URL = 'https://api.zeno.fm/mounts/metadata/subscribe/n4gzbe9ufzzuv';
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
